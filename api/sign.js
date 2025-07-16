68e7fdd3bccab00f66aed1c7aca9ce5d0332bf59
tapdopgames <tapdopstudios@gmail.com>
2025-07-16 16:39:30 +0530

    Update sign.js

--- api/sign.js
+++ api/sign.js
@@ -1,1 +1,15 @@
+// api/sign.js
+export default function handler(req, res) {
+  const crypto = require("crypto");
+
+  const { timestamp, uploadPreset } = req.query;
+  const secret = process.env.CLOUDINARY_API_SECRET;
+
+  const signature = crypto
+    .createHash("sha1")
+    .update(`timestamp=${timestamp}&upload_preset=${uploadPreset}${secret}`)
+    .digest("hex");
+
+  res.status(200).json({ signature });
+}
 
